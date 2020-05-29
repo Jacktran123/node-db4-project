0309b3d93cd8f3219b655fdddf6a85f9b2f41bdc
Khoa Tran <jacktran1799@gmaail.com>
2020-05-28 23:43:19 -0500

    finish MVP, will try to work on stretch

--- server/data/migrations/20200528200422_create-recipes.js
+++ server/data/migrations/20200528200422_create-recipes.js
@@ -1,7 +1,51 @@
 exports.up = function (knex) {
-   
+   return (
+       knex.schema.createTable('Recipes', tbl=>{
+       tbl.increments('id');
+       tbl.text('Recipe_Name',250).notNullable().unique();
+    })
+    .createTable('Ingredients', tbl=>{
+        tbl.increments('id');
+        tbl.text('Ingredient',250).notNullable().unique();
+    })
+    .createTable('Recipes_Book', tbl=>{
+        tbl.integer('Recipe_Id',14)
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('Recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+        tbl.integer('Ingredient_Id',14)
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('Ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+        tbl.text('Quantity',13)
+        .unsigned()
+        .notNullable()
+    })
+    .createTable('Instructions',tbl=>{
+        tbl.increments('id');
+        tbl.integer('Recipe_Id',3)
+        .notNullable()
+        .unsigned()
+        .references('Recipes.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+        tbl.text('Instruction',123).notNullable()
+        tbl.integer('Step_Number',12)
+        .notNullable()
+    })
+   );
 };
   
 exports.down = function (knex) {
-   
+   return knex.schema
+   .dropTableIfExists('Instructions')
+   .dropTableIfExists('Recipes_Book')
+   .dropTableIfExists('Ingredients')
+   .dropTableIfExists('Recipes')
 };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const helmet=require('helmet');
 const morgan=require('morgan');
+const RecipeApi=require('./api/Recipe-api');
 
 const server=express();
 
@@ -8,6 +9,8 @@ const server=express();
 server.use(express.json());
 server.use(morgan());
 server.use(helmet());  
+server.use('/api/', RecipeApi);
+
 
 
 
